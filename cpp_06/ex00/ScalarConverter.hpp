232f95d510a455bd4b19012e882fac735f161f74
xAn4ss <13anas.oukharta37@gmail.com>
2023-04-18 18:07:04 +0000

    working on cpp06/ex00

--- cpp_06/ex00/ScalarConverter.hpp
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -3,6 +3,15 @@
 
 #include <iostream>
 
+
+enum input_type{
+    INT,
+    CHAR,
+    FLOAT,
+    DOUBLE,
+    LITERALS
+};
+
 class ScalarConverter
 {
 private:
@@ -10,10 +19,17 @@ private:
     float _float;
     double _double;
     std::string _char;
-
+    input_type  _type;
     ScalarConverter(std::string input);
+    
 public:
-    void Convert(std::string _literal);
+    void        Convert(std::string input);
+    input_type  getType(std::string);
+    bool        isInt(std::string input);
+    bool        isChar(std::string input);
+    bool        isFloat(std::string input);
+    bool        isDouble(std::string input);
+    bool        isLiteral(std::string input);
     ~ScalarConverter();
 };
 
@@ -27,9 +43,24 @@ ScalarConverter::~ScalarConverter()
 
 }
 
-void ScalarConverter::Convert(std::string target)
+input_type ScalarConverter::getType(std::string input){
+
+    if (isInt(input))
+        return INT;
+    else if (isChar(input))
+        return CHAR;    
+    else if (isFloat(input))
+        return FLOAT;
+    else if (isDouble(input))
+        return DOUBLE;
+    else if (isLiteral(input))
+        return LITERALS;
+}
+
+void ScalarConverter::Convert(std::string input)
 {
-    
+
+    this->_type = this->getType();
 }
 
 
