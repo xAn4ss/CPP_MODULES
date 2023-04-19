1a89bec0e526375aeb384b7bbc539606d8c29339
xAn4ss <13anas.oukharta37@gmail.com>
2023-04-19 18:56:53 +0000

    .

--- cpp_06/ex00/ScalarConverter.hpp
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -2,7 +2,7 @@
 #define SCALARCNVERTER_HPP
 
 #include <iostream>
-
+#include <cstring>
 
 enum input_type{
     INT,
@@ -26,11 +26,11 @@ public:
     static void        Convert(std::string input);
     static input_type  getType(std::string);
     static void        setType(input_type type);
-    static bool        isInt(std::string input);
-    static bool        isChar(std::string input);
-    static bool        isFloat(std::string input);
-    static bool        isDouble(std::string input);
-    static bool        isLiteral(std::string input);
+    bool        isInt(std::string input);
+    bool        isChar(std::string input);
+    bool        isFloat(std::string input);
+    bool        isDouble(std::string input);
+    bool        isLiteral(std::string input);
     static void        convertToInt(std::string input);
     ~ScalarConverter();
 };
@@ -54,9 +54,26 @@ bool ScalarConverter::isInt(std::string input){
     long nb = atol(input.c_str());
     if (nb > INT32_MAX || nb < INT32_MIN)
         return false;
-    input.end
-    std::cout << nb << std::endl;
+    if (input.back() == 'f' && input.find(".", 0) != -1)
+        return false;
+    _int = static_cast<int>(nb);
+    return true;
 }
+
+bool ScalarConverter::isChar(std::string input)
+{
+    if (!strcmp(input.c_str(), "nan") || !strcmp(input.c_str(), "nanf") ||
+        !strcmp(input.c_str(), "+inf") || !strcmp(input.c_str(), "+inff") ||
+        !strcmp(input.c_str(), "-inf") || !strcmp(input.c_str(), "-inff"))
+            return false;
+    if (!isprint(input[0]))
+    {
+        _char = "Non displayable";
+        
+    }
+    return true;
+}
+
 // input_type ScalarConverter::getType(std::string input){
 
 //     if (isInt(input))
@@ -81,6 +98,7 @@ void ScalarConverter::convertToInt(std::string input){
 
 // void ScalarConverter::Convert(std::string input)
 // {
+        // _int = 0;
 
 //     _type = getType(input);
 //     switch (_type)
