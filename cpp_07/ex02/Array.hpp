f8c4f5654e520c1d7cf292e149400c323af3a80d
xAn4ss <13anas.oukharta37@gmail.com>
2023-04-29 19:14:18 +0100

    ex02

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -1,1 +1,36 @@
-#ifndef
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template<class T> class Array
+{
+    private:
+        T arr[];
+    public:
+        Array()
+        {
+            arr = NULL;
+        };
+        Array(unsigned int n){
+            int *a = new T(n);
+        };
+        Array(const &Array copied)
+        {
+            
+        };
+        Array& operator=(const &Array rval)
+        {
+            if (this != &rval)
+            {
+                
+            }
+            return 
+        };
+        ~Array()
+        {
+            delete arr[];
+        };
+};
+
+
+
+#endif
---
+++ cpp_07/ex02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    int *a = new int();
+    std::cout << a << std::endl;
+    return 0;
+}
