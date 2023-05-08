31d1b15b5ff35fda1dc85f5aaceef560ddb8e541
xAn4ss <13anas.oukharta37@gmail.com>
2023-05-09 00:12:09 +0100

    ex01

--- cpp_09/ex01/main.cpp
+++ cpp_09/ex01/main.cpp
@@ -10,7 +10,8 @@ int main(int ac, char ** av){
     std::stringstream s(av[1]);
     std::list<int> data;
     std::string buf;
-    std::list<int>::iterator it;
+    std::list<int>::iterator it, it2;
+    int result = 0;
     while (getline(s, buf, ' '))
     {
         if (buf.size() != 1)
@@ -21,16 +22,50 @@ int main(int ac, char ** av){
         if (isdigit(buf[0]))
         {
             data.push_back(atoi(buf.c_str()));
+            // std::cout << "-----------" <<std::endl;
+            // for (it = data.begin(); it != data.end(); it++)
+            // {
+            //     std::cout << *it << " ";
+            // }
+            // std::cout << std::endl;
         }
         else if (buf[0] == '+' || buf[0] == '-' || buf[0] == '*' || buf[0] == '/')
         {
-            switch
+            it = data.end()--;
+            switch (buf[0])
+            {
+                case '+':
+                    result = *it + *++it;
+                    break;
+                case '-':
+                    result = *it - *++it;
+                    break;
+                case '*':
+                    result = *it * *++it;
+                    break;
+                case '/':
+                    result = *it / *++it;
+                    break;
+            }
+            it = data.end();
+            it--;
+            it2 = data.end();
+            std::cout << *--it << " " << buf[0] << " " << *--it2 << " --> " << result << std::endl;
+            data.pop_back();
+            data.pop_back();
+            data.push_back(result);
         }
-    }
-    for (it = data.begin(); it != data.end(); it++)
-    {
-        std::cout << *it << std::endl;
+            for (it = data.begin(); it != data.end(); it++)
+            {
+                std::cout << *it << " ";
 
+            }
+            std::cout << std::endl;
+            // std::cout << "************" <<std::endl;
+            // std::cout << std::endl;    
+            // std::cout << (*data.end() - 1) << " . " << *data.end() << std::endl;
     }
+    std::cout << "---> " <<*data.begin()<< " / " << result << std::endl;
+
     return 0;
 }
